a9eb4d162f960dbe23dda14eda786cdaa4ac80bc
almaaro <alvar.martti@gmail.com>
2020-06-01 23:20:53 +0300

    turn load factor corrections to throttle/ste rate demands

--- tecs/tecs.h
+++ tecs/tecs.h
@@ -399,7 +399,7 @@ private:
 	/**
 	 * Calculate specific total energy rate limits
 	 */
-	void _update_STE_rate_lim(float throttle_cruise);
+        void _update_STE_rate_lim(float throttle_cruise, const matrix::Dcmf &rotMat);
 
 	/**
 	 * Initialize the wing's lift profile calculations
